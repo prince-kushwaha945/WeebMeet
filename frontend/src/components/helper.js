a86661d68ac3986bd1d0db75559e9d59ac5fdff2
Coderprince660 <kushwahaprince660@gmail.com>
2024-07-22 10:28:26 +0530

    Fixed file issues

---
+++ frontend/src/components/helper.js
@@ -0,0 +1,2 @@
+export const appId = 984664698;
+export const serversecret = "ebabea34180039e0bd01ab4a749040d5";
--- src/components/helper.js
+++
@@ -1,2 +0,0 @@
-export const appId = 908292249;
-export const serversecret = "e1ad0c024b7ad53bb616ed2bf852ec6d";
